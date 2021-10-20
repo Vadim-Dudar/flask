9c38ade49d3300edf6ed0314a59e58848cd4a2f2
Vadim Dudar <haskervadim228@gmail.com>
2021-10-20 19:47:24 +0300

    add db, teamplate

--- market.py
+++ market.py
@@ -3,15 +3,29 @@
 # export FLASK_APP=market.py
 # flask run
 
-
+from enum import unique
+from operator import length_hint
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(), nullable=False)
 
 
 @app.route('/')
-def home_page():
+def home():
     return render_template('home.html')
 
 @app.route('/about')
