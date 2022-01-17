e33a6888693cc1ba87b8dced12dfd64e48d73953
”Vadim <haskervadim228@gmail.com>
2022-01-17 12:12:28 +0200

    add registration form

--- market/__init__.py
+++ market/__init__.py
@@ -7,8 +7,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '6207f8257d6110f6fbd5d689'
 db = SQLAlchemy(app)
 
 
--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
-from market import app
-from flask import render_template
-from market.models import Item
+from market import app, db
+from flask import render_template, redirect, url_for
+from market.models import Item, User
+from market.forms import RegisterForm
 
 
 @app.route('/')
@@ -22,3 +23,19 @@ def about_user(user):
 def market():
     items = Item.query.all()
     return render_template('market.html', items=items)
+
+
+@app.route('/register', methods=['get', 'post'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        create_user = User(username=form.username.data,
+                           email_address=form.email_address.data,
+                           password_hash=form.password1.data)
+        db.session.add(create_user)
+        db.session.commit()
+        return redirect(url_for('market'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print('!', err_msg)
+    return render_template('register.html', form=form)
